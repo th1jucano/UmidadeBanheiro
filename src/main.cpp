faef20ff9c7104183392ea0e7e3806e984f7c781
th1jucano <thiago.corporative@gmail.com>
2026-08-27 12:05:14 -0300

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <PiscaLed.h>
+
+int rele = 8;
+int sensor = 10;
+float somaLeituras = 0;
+float qtdLeituras = 0;
+float media = 0;
+int pinoLed = 13;
+
+DHT dht(sensor, DHT11);
+PiscaLed led(13);
+
+void setup() {
+  dht.begin();
+  Serial.begin(9600);
+  pinMode(rele, OUTPUT);
+  pinMode(sensor, INPUT);
+  led.begin();
+}
+
+void loop() {
+  float umidade = dht.readHumidity();
+  somaLeituras = somaLeituras + umidade;
+  qtdLeituras = qtdLeituras + 1;
+  media = somaLeituras / qtdLeituras;
+  Serial.print("Umidade: ");
+  Serial.println(umidade);
+  Serial.print("Media ate o momento: ");
+  Serial.println(media);
+  delay(1000);
+  led.atualizar();
+
+}
